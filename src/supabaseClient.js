173a6794688e876c44e7432a7d65c7155cef8a78
asan <kussaiynov.assanali99@gmail.com>
2026-09-18 00:55:20 +0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qkcknohwkgcsxduweoyv.supabase.co';
-const supabaseAnonKey = 'sb_publishable_W9T-xstG7unSUljmq3dJDQ_yX723gKP';
+// Подключение к твоему проекту Supabase
+const supabaseUrl = 'https://ТВОЙ_ПРОЕКТ.supabase.co';
+const supabaseKey = 'ТВОЙ_ANON_KEY';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
