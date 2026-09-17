df26b5bb5fbb57794f3c67da2f5a1ee7e0a9ec9e
asan <kussaiynov.assanali99@gmail.com>
2026-09-17 16:16:26 +0500

    Create almatyGyms.js

---
+++ src/data/almatyGyms.js
@@ -0,0 +1,288 @@
+// Полная скомпонованная база залов и студий Алматы из 2GIS
+export const ALMATY_GYMS = [
+  // Свободный выбор / Агрегаторы
+  "1Fit Pass (Единый абонемент во все залы)",
+  "Любой зал в городе / Ищу компанию",
+
+  // === INVICTUS FITNESS & GO ===
+  "Invictus Go | ЖК Oner Towers, проспект Абая, 165 блок 9",
+  "Invictus Go | Улица Тимирязева, 42",
+  "Invictus Go | Улица Навои, 97",
+  "Invictus Go | Улица Аскарова, 4/3",
+  "Invictus Go | ЖК LAMIYA, улица Ади Шарипова, 145 к2",
+  "Invictus Go | ТРЦ Riviera Park, улица Каныша Сатпаева, 90/21",
+  "Invictus Go | ЖК Megapolis, микрорайон Мамыр-1, 26/1",
+  "Invictus Go | ЖК Forum Residence, улица Байтурсынова, 179 блок 2",
+  "Invictus Go | Улица Саина, 16а",
+  "Invictus Go | Микрорайон Аксай-5, 25",
+  "Invictus Go | Улица Каракулова, 39",
+  "Invictus Go | ЖК Royal, улица Кажымукана, 49",
+  "Invictus Go | ЖК Lifetown, улица Торекулова, 93 блок 14",
+  "Invictus Go | Улица Маметовой, 54",
+  "Invictus Fitness Gagarin | Проспект Гагарина, 286",
+  "Invictus Fitness Sadu | Проспект Аль-Фараби, 128/7",
+  "Invictus Fitness | Микрорайон Самал-1, 9а",
+  "Invictus Girls | Проспект Абая, 160 к3/1",
+
+  // === ADRENALINE ===
+  "Adrenaline | Проспект Жибек Жолы, 66",
+  "Adrenaline | Касымова улица, 32",
+  "Adrenaline | ЖК Ботанический Бульвар, улица Ауэзова, 163а",
+  "Adrenaline | Микрорайон Жетысу-2, 69Б",
+  "Adrenaline | БЦ Aspara, проспект Райымбека, 348/1",
+  "Adrenaline | Проспект Назарбаева, 223",
+  "Adrenaline | ЖК Maxima Residence, улица Кабанбай батыра, 49/1 блок D1",
+  "Adrenaline | ЖК Этюд, проспект Достык, 250",
+
+  // === БАНЗАЙ FITNESS & IMPULSE ===
+  "БАНЗАЙ Fitness | Проспект Абая, 150",
+  "БАНЗАЙ Fitness | ТРК Atakent Mall, улица Тимирязева, 42 к3",
+  "БАНЗАЙ Fitness | Улица Толе би, 187 к2",
+  "БАНЗАЙ Fitness | ТРЦ DOMILLION, проспект Турара Рыскулова, 143в",
+  "БАНЗАЙ Fitness | Улица Кунаева, 43",
+  "БАНЗАЙ Fitness-Premium Samal | ТРЦ Ритц-Палас, проспект Аль-Фараби, 1",
+  "БАНЗАЙ IMPULSE | Улица Толе би, 187 к2",
+
+  // === YA. & YA. PILATES ROOM ===
+  "YA. | Улица Навои, 200 блок 1",
+  "YA. | ТРЦ Riviera Park, улица Каныша Сатпаева, 90/21",
+  "YA. | Улица Минусинская, 13Б",
+  "YA. | ТРЦ SPUTNIK mall, микрорайон Мамыр-1, 8а",
+  "YA. | ЖК Куат на Достык-Сатпаева, проспект Достык, 116",
+  "YA.Gym Point | ТД Таугуль, улица Пятницкого, 15/16",
+  "YA. Pilates Room | Улица Навои, 200 блок 2",
+  "YA. Pilates Room | ЖК Apple Residence, улица Аскарова, 12",
+  "YA. Pilates Room | ЖК Esentai City, проспект Аль-Фараби, 116/20",
+  "YA. Pilates Room | Globus, проспект Абая, 109в",
+  "YA. Pilates Room | ТРЦ Riviera Park, улица Каныша Сатпаева, 90/21",
+
+  // === 35 HEALTH CLUBS KAZAKHSTAN ===
+  "35 Health Clubs Kazakhstan | ЖК Флэтхаус Freedom, улица Тургут Озала, 261",
+  "35 Health Clubs Kazakhstan | ЖК City Plus, улица Толе би, 285/8 к5",
+  "35 Health Clubs Kazakhstan | ЖК Тау Терек, микрорайон Мамыр-3, 23",
+  "35 Health Clubs Kazakhstan | Проспект Серкебаева, 99 блок 5",
+  "35 Health Clubs Kazakhstan | ЖК LAMIYA, улица Ади Шарипова, 145 к2",
+  "35 Health Clubs Kazakhstan | ЖК Forum Plus, улица Володарского, 40а",
+  "35 Health Clubs Kazakhstan | ЖК Шахристан, улица Навои, 208",
+  "35 Health Clubs Kazakhstan | Улица Толе би, 75",
+  "35 Health Clubs Kazakhstan | Проспект Райымбека, 162а блок 9",
+
+  // === PYRAMID ===
+  "Pyramid Gym | Микрорайон Аксай-5, 24",
+  "Piramid Strong Pro | Микрорайон Мирас, 128/5",
+  "Pyramid pride | ЖК Бакытты омир, проспект Жибек Жолы, 131",
+  "Pyramid renessans | ЖК Ренессанс, проспект Назарбаева, 301",
+  "Pyramid renesans | Проспект Абая, 202/1",
+  "Pyramid Hard | Проспект Абая, 202/1",
+  "PG Fitness terra | Улица Тлендиева, 215",
+  "PG Fitness | Terra, проспект Серкебаева, 91 блок D",
+
+  // === WORKOUT ===
+  "WORKOUT | ТД Дукат, 10-й микрорайон, 3Б",
+  "WORKOUT | ТРК Forum, проспект Сейфуллина, 617",
+  "WORKOUT | БЦ Taurus, улица Жарокова, 124",
+  "WORKOUT | Улица Аскарова, 10а",
+
+  // === AQUASTARS ===
+  "Aquastars | ЖК Шахристан, улица Навои, 208/6",
+  "Aquastars | Улица Гоголя, 114 к8",
+  "Aquastars | Улица Яссауи, 15/2",
+  "Aquastars | ЖК Столичный Центр, улица Кабанбай батыра, 87",
+  "Aquastars | Улица Жамакаева, 256а",
+
+  // === HERO'S JOURNEY ===
+  "Hero's Journey | МФК VILLA, проспект Аль-Фараби, 140а",
+  "Hero's Journey | Улица Ескараева, 3",
+  "Hero's Journey | TPU PROMENADE, проспект Абая, 44а",
+  "Hero's Journey | Улица Уалиханова, 170/1",
+
+  // === ROYAL CLUB ===
+  "Royal club | Ботанический Сад улица, 26",
+  "Royal club | ЖК Бухар Жырау Тауэрс, Бухар жырау бульвар, 27/5",
+  "Royal club | Микрорайон Самал-3, 20",
+  "Royal club | Нурлы-Тау, проспект Аль-Фараби, 7 к5а",
+
+  // === S89 FITNESS ===
+  "S89 Fitness | Улица Радостовца, 69",
+  "S89 Fitness Clubs | Проспект Гагарина, 244а",
+  "S89 Fitness Clubs | БЦ Green Hill, проспект Суюнбая, 89Б",
+
+  // === 100% FITNESS GYM ===
+  "100% Fitness Gym | БЦ Almaty Towers, Байзакова улица, 280",
+  "100% Fitness Gym | Улица Кожамкулова, 136",
+  "100% Fitness Gym | ЖК Раушан, улица Кабанбай батыра, 147",
+  "100% Fitness Gym | ТД на зеленом, улица Макатаева, 45",
+
+  // === FITNESSBLITZ ===
+  "FitnessBlitz | Микрорайон Самал-1, 9а",
+  "FitnessBlitz | Микрорайон Аксай-5, 3",
+  "FitnessBlitz | ЖК Dial Residence, проспект Сейфуллина, 533",
+
+  // === BALANCE ===
+  "Balance | Улица Навои, 280",
+  "Balance | ЖК Assem plaza, улица Ауэзова, 5",
+  "Balance | БЦ Толе би, улица Толе би, 101",
+
+  // === PROSTRETCHING ===
+  "Prostretching | 5-й микрорайон, 30Б",
+  "Prostretching | ЕЮА им. Д.А. Кунаева, улица Курмангазы, 107",
+  "Prostretching | Улица Тимирязева, 42 к10а",
+
+  // === BUBBLZZ ===
+  "Bubblzz | ЖК Мечта на Минина, улица Минина, 24",
+  "Bubblzz | Улица Амангельды, 23/137",
+  "Bubblzz | БЦ Коктем Grand, проспект Достык, 210",
+
+  // === VIVA PILATES ===
+  "ViVa Pilates | БЦ Almaty Residence, улица Ауэзова, 60",
+  "ViVa Pilates | Микрорайон Астана, 1/12",
+
+  // === POWER PILATES ===
+  "Power Pilates | БЦ Алатау Гранд, улица Тимирязева, 28в",
+  "Power Pilates | ЖК Ренессанс, проспект Назарбаева, 301",
+
+  // === UNDERGROUND ===
+  "Underground Big | Проспект Абая, 150",
+  "Underground Fit | Улица Торайгырова, 21/1Б",
+
+  // === PROGYM ===
+  "ProGym Sky | БЦ Гермес, улица Муратбаева, 180",
+  "Progym | Улица Карасай батыра, 88Б",
+  "Progym | Улица Калдаякова, 17",
+
+  // === ESENTAI FIT+SPA ===
+  "Esentai Fit+Spa | TU ESENTAI MALL, проспект Аль-Фараби, 77/8 (Корпус 1)",
+  "Esentai Fit+Spa | TU ESENTAI MALL, проспект Аль-Фараби, 77/8 (Корпус 2)",
+
+  // === AQONGYM ===
+  "Aqongym | Микрорайон Жетысу-2, 11/1",
+  "Aqongym Air | Микрорайон Жетысу-1, 16/4",
+
+  // === 31 PILATES STUDIO ===
+  "31 Pilates Studio | Проспект Гагарина, 310/1",
+  "31 Pilates Studio | ЖК Терренкур, проспект Достык, 128",
+
+  // === FIRST PILATES STUDIO ===
+  "First Pilates Studio | ЖК Гаухартас, проспект Абая, 150/230 блок 4",
+
+  // === ОСТАЛЬНЫЕ ЗАЛЫ И СТУДИИ ===
+  "19hills | ЖК Nur-ay Residence, улица Шевченко, 96",
+  "777 Fight Club & Gym | ЖК Центральный, улица Курмангазы, 97",
+  "Academy Woman Forum | ТРК Forum, проспект Сейфуллина, 617",
+  "AF Sport | Улица Навои, 300",
+  "Almaly Fitness | Керей-Жанибек хандар улица, 103Б",
+  "Almaty Resort Pool | Санаторий Almaty Resort, улица Альмерек, 1/1",
+  "Alta pole studio | Улица Толе би, 189д",
+  "Amane | ЖК Metropole, проспект Аль-Фараби, 41/7 блок 7",
+  "AMG | Микрорайон Аксай-4, 70Б",
+  "Angel | ЖК Royal, улица Кажымукана, 49",
+  "Ankara | Гостиница InterContinental Almaty, улица Желтоксан, 181",
+  "Ant Gym | ЖК Алтын Орда, улица Ходжанова, 81 блок 3",
+  "Aquamarine SPA & Fitness | Гостиница Ramada Almaty, улица Байтурсынова, 27/1",
+  "Arena fitness hub | ALMATY ARENA, микрорайон Нуркент, 7",
+  "Argo fitness | БЦ Карасай, улица Карасай батыра, 152/1",
+  "Arlan Black Wolves | Улица Наурызбай батыра, 91Б",
+  "Aru ana studio | Улица Розыбакиева, 320",
+  "Asyl Gym | ТЦ Асыл, улица Туркебаева, 92",
+  "Avantgarde | ЖК Науаи, улица Навои, 7",
+  "Balance and Grace Pilates Studio | Улица Кажымукана, 10а",
+  "Baqgym | Улица Яссауи, 66а",
+  "Be Turbo | БЦ Алатау Гранд, улица Тимирязева, 28в",
+  "BeFitness | АФЭК, улица Рыскулбекова, 39а",
+  "Best Gym Almaty | Улица Байжанбаева, 5",
+  "Bodyfitgym | 7-й микрорайон, 3а",
+  "Boss Tangym | Улица Маметовой, 67",
+  "Brooklyn Fitness Gym | ЖК Керемет, микрорайон Керемет, 7 к42",
+  "Budokan Sports & Arts | Улица Сагадат Нурмагамбетов, 150",
+  "Champion | Улица Кожабекова, 6",
+  "Comfort Stretch | Улица Торайгырова, 41",
+  "Cordial Fitness | Микрорайон Мамыр-7, 21",
+  "D-fitness | 12-й микрорайон, 20а",
+  "Dance Studio 29 | Улица Гоголя, 84а",
+  "Elastic Stretch Studio | Микрорайон Жетысу-2, 52/1",
+  "Elastic studio | ЖК Nest Grand, микрорайон Астана, 1/18 к7",
+  "Ems Fit x Body | Улица Жубанова, 3а",
+  "Endurance Almaty | Улица Байсеитовой, 49",
+  "Eyva Space | ЖК Seven Hills, проспект Серкебаева, 101",
+  "Family Space | Микрорайон Самал-2, 91",
+  "Feel fit club | Улица Катаева, 158а",
+  "Fightclub.kz | БЦ Премьер Алатау, проспект Достык, 105",
+  "First fitness | Микрорайон Таугуль-1, 22/1",
+  "Flower Dance | Микрорайон Жетысу-3, 65",
+  "Forme | ЖК Metropole, проспект Аль-Фараби, 41/7 блок 8",
+  "Formula fitness & sport studio | 1-й квартал, 12а, с. Жанатурмыс",
+  "Gepard Sport Club | Улица Утеген батыра, 11а",
+  "Glide | Проспект Аль-Фараби, 120/53",
+  "Grandpool | Ораза Жандосова улица, 55/9",
+  "Gym arena | Рынок Турксиб, проспект Суюнбая, 261а",
+  "Hawaii Fitness | Молл Апорт, Ташкентский тракт, 17к",
+  "In Clover | Улица Курмангазы, 95",
+  "Iron House | Улица Шевченко, 100",
+  "IronGym | Улица Каныша Сатпаева, 30/2в",
+  "K1 Fitness | ТРК ADK, улица Каныша Сатпаева, 90",
+  "Kaktus Fit | Проспект Абылай хана, 167, Каскелен",
+  "Kinesis Gym | Микрорайон Жетысу-3, 67",
+  "King Fitness | Улица Коктерек, 141",
+  "KRIDA Premium Fitness Club | Улица Наурызбай батыра, 89",
+  "L-fitness | ТЦ Дархан, улица Айманова, 155",
+  "La Vida Pilates | Проспект Гагарина, 245 к5",
+  "La Vida Pilates | Микрорайон Казахфильм, 41а",
+  "Le Ciel | Улица Тимирязева, 67а",
+  "LegenDa | Улица Торайгырова, 39",
+  "Legion Fitness | ТЦ Алатау, проспект Турара Рыскулова, 103/3",
+  "Let's jump | Микрорайон Жетысу-2, 47а",
+  "Level fitness & Gym | БЦ Премьер Алатау, проспект Достык, 105",
+  "Life Fitness Center | Улица Розыбакиева, 273",
+  "Lifestyle | Улица Исиналиева, 1/1",
+  "Materia | Улица Розыбакиева, 159а",
+  "Medina Fitness Studio | Улица Егизбаева, 13/3",
+  "MegaGym | Улица Абиша Кекилбайулы, 38а/1",
+  "Millennium | ЖК Алтын Булак-1, Е. Брусиловского улица, 159 блок 4",
+  "Mind Of Body | Улица Наурызбай батыра, 10а",
+  "MV movement studio | Улица Тимирязева, 42 пав17",
+  "Nautilus | ЖК Шахристан, улица Навои, 208/6",
+  "Nomad Gym | Микрорайон Жетысу-1, 47",
+  "Ober Pilates Studio | ЖК Гаухартас, проспект Абая, 150/230 блок 6",
+  "Ocean Soul | ЖК Metropole, проспект Аль-Фараби, 41/6 блок 15",
+  "Olive Pilates | ЖК Тенгиз Тауэрс, улица Каныша Сатпаева, 30/2",
+  "Profitnessamg | Улица Каныша Сатпаева, 33",
+  "Projumping Fitness | Микрорайон Аксай-1, 10/3",
+  "Prosto_fitspace | БЦ Желтоксан, улица Желтоксан, 111а",
+  "PULS studio | ЖК Алма-Ата, проспект Гагарина, 66Б",
+  "Pulse | MOK Samal Mall, проспект Назарбаева, 226",
+  "Q Fitness | Республики площадь, 15",
+  "Rakhat Fitness | Проспект Абая, 48",
+  "Reboot Gym Studio | Проспект Сейфуллина, 574/3",
+  "Select dance & Fit | Проспект Абая, 130/3",
+  "Siam | Улица Азербаева, 67",
+  "SK boxing | ЖК Импорио, улица Тимирязева, 111а",
+  "Sport Line Z | 3-й микрорайон, 7а",
+  "Stretching Almaty | Улица Панфилова, 92",
+  "Studio 191 | Академия тенниса Максат, улица Абиша Кекилбайулы, 191",
+  "Tamos Family Club | Улица Какимбека Салыкова, 50/1",
+  "TauFit | Жилой комплекс Тау Самалы, 98",
+  "Technofit | ЖК Дом на Абая, проспект Гагарина, 124",
+  "THE SPA RIXOS ALMATY | Гостиница Rixos Almaty, проспект Сейфуллина, 506",
+  "Uniflex | Микрорайон Жетысу-2, 2а",
+  "Uniflex Women | Микрорайон Жетысу-2, 2а",
+  "Uprime | ЖК Опера, микрорайон Аксай-4, 119",
+  "Urban Gym | Проспект Назарбаева, 223",
+  "Vavilon Family Sport Club | ЖК Вавилон, улица Розыбакиева, 247 блок 5",
+  "Velvet Sport Villa | Микрорайон Ерменсай 3, 23",
+  "Versatile Fit-Boxing | Улица Нурмакова, 4",
+  "Wave | Улица Наурызбай батыра, 127 блок 1",
+  "Wellness Club LUXOR | Проспект Достык, 341",
+  "Winox Pro | ТЦ Jurek Таи, проспект Аль-Фараби, 5/2",
+  "Woom | ЖК Abai, проспект Сейфуллина, 597/7",
+  "Yfitness & Spa | Улица Герольда Бельгера, 50",
+  "Ya Kids | БЦ Rams Qazaqstan, улица Жамбыла, 106",
+  "Zeyin | 12-й микрорайон, 15а",
+  "Zhailjau | ЖК Жайлау, микрорайон Мирас, 188/2",
+  "Zip Fit | ЖК Солнечный квартал, улица Бальзака, 8 лит В",
+  "Пляж | Улица Карасай батыра, 120а",
+  "Сфера | Микрорайон Самал-2, 66а",
+  "Тастак | Улица Толе би, 249-249а",
+
+  // Дополнительная опция ручного ввода
+  "Другой зал (указать в профиле)"
+];
