e0983dd746d41eb1458a11381e8fa2b86d1fe7fe
asan <kussaiynov.assanali99@gmail.com>
2026-09-19 04:21:12 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/gymconnect/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
