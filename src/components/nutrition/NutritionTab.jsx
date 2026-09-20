d31291aa162670894218a76bd6ea7b451ee2444a
asan <kussaiynov.assanali99@gmail.com>
2026-09-20 07:36:48 +0500

    Update NutritionTab.jsx

--- src/components/nutrition/NutritionTab.jsx
+++ src/components/nutrition/NutritionTab.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import NutritionHeader from './NutritionHeader';
-// остальные импорты...
+import NutritionDisclaimer from './NutritionDisclaimer';
+import CalorieCalculator from './CalorieCalculator';
+import WaterTracker from './WaterTracker';
+import AllergySelector from './AllergySelector';
+import DietPlan from './DietPlan';
+import GroceryCart from './GroceryCart';
+import SupplementsShop from './SupplementsShop';
 
 export default function NutritionTab() {
   return (
     <div className="p-3 max-w-md mx-auto pb-24 animate-in fade-in duration-200">
       <NutritionHeader />
-      {/* остальной контент питания */}
+      <NutritionDisclaimer />
+      <CalorieCalculator />
+      <WaterTracker />
+      <AllergySelector />
+      <DietPlan />
+      <GroceryCart />
+      <SupplementsShop />
     </div>
   );
 }
