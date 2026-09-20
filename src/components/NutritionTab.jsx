cb68321dd11ad8566647ebf8184d6ef8a9af1db7
asan <kussaiynov.assanali99@gmail.com>
2026-09-20 05:38:18 +0500

    Delete src/components/NutritionTab.jsx

--- src/components/NutritionTab.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NutritionTabRoot from './nutrition/NutritionTab';
-
-export default function NutritionTab({ myProfile, onUpdateProfile }) {
-  return <NutritionTabRoot myProfile={myProfile} onUpdateProfile={onUpdateProfile} />;
-}
