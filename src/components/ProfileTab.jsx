6b8451c76e012bbc596a85ce9af0b257c3b92f2b
asan <kussaiynov.assanali99@gmail.com>
2026-09-20 05:38:27 +0500

    Delete src/components/ProfileTab.jsx

--- src/components/ProfileTab.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ProfileTabRoot from './profile/ProfileTab';
-
-export default function ProfileTab({ user, onUpdateUser, onNavigateTab }) {
-  return <ProfileTabRoot user={user} onUpdateUser={onUpdateUser} onNavigateTab={onNavigateTab} />;
-}
