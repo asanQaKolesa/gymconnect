a70e2efb0b008afd9ea8965a16b83aec3c17552d
asan <kussaiynov.assanali99@gmail.com>
2026-09-20 07:43:53 +0500

    Update ProfileTab.jsx

--- src/components/profile/ProfileTab.jsx
+++ src/components/profile/ProfileTab.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProfileHeader from './ProfileHeader';
+import ProfileCard from './ProfileCard';
 import ProfileMenu from './ProfileMenu';
 import ProfileDocs from './ProfileDocs';
 import ProfileDangerZone from './ProfileDangerZone';
@@ -8,6 +9,7 @@ export default function ProfileTab() {
   return (
     <div className="p-4 max-w-md mx-auto pb-24 animate-in fade-in duration-200">
       <ProfileHeader />
+      <ProfileCard />
       <ProfileMenu />
       <ProfileDocs />
       <ProfileDangerZone />
