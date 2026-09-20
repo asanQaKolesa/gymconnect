6fdabe43d62cbdead4c739e9d47b02d3bfbce930
asan <kussaiynov.assanali99@gmail.com>
2026-09-20 06:17:27 +0500

    Update NutritionTab.jsx

--- src/components/nutrition/NutritionTab.jsx
+++ src/components/nutrition/NutritionTab.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import NutritionDisclaimer from './NutritionDisclaimer';
 import NutritionCalculator from './NutritionCalculator';
+import NutritionAllergies from './NutritionAllergies';
 import WaterTracker from './WaterTracker';
 import NutritionDiet from './NutritionDiet';
 import NutritionCart from './NutritionCart';
 import NutritionSupplements from './NutritionSupplements';
-import NutritionDisclaimer from './NutritionDisclaimer';
 
 export default function NutritionTab() {
   return (
     <div className="p-4 max-w-md mx-auto pb-24 animate-in fade-in duration-200">
       <h1 className="text-xl font-bold text-slate-900 mb-4 px-1">Питание и КБЖУ</h1>
       
-      {/* Дисклеймер на самомверху для максимальной видимости */}
       <NutritionDisclaimer />
-      
       <NutritionCalculator />
+      <NutritionAllergies />
       <WaterTracker />
       <NutritionDiet />
       <NutritionCart />
