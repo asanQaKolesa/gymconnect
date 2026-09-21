358cc73ed2d2e54a2d6ffb80650b5a3595d7556a
asan <kussaiynov.assanali99@gmail.com>
2026-09-22 01:39:48 +0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
+// src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-// Подключение к твоему проекту Supabase
-const supabaseUrl = 'https://ТВОЙ_ПРОЕКТ.supabase.co';
-const supabaseKey = 'ТВОЙ_ANON_KEY';
+const supabaseUrl = 'https://qkcknohwkgcsxduweoyv.supabase.co'; // твой URL из панели Supabase
+const supabaseKey = 'sb_publishable_W9T-xstG7unSUljmq3dJDQ_yX723gKP'; // твой новый Publishable key
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
